e6aa264b2988f5bf77ede42d9aec5aa52a20708b
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2022-10-29 23:49:39 +0100

    test

--- workshop/src/app/admin/admin-routing.module.ts
+++ workshop/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateUserComponent } from './update-user/update-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "updateUser", component: UpdateUserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- workshop/src/app/admin/admin.module.ts
+++ workshop/src/app/admin/admin.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { UpdateUserComponent } from './update-user/update-user.component';
 
 
 @NgModule({
   declarations: [
     
+  
+    UpdateUserComponent
   ],
   imports: [
     CommonModule,
